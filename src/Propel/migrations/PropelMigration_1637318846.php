85a0eb603fdd83491c28fe92d76a1b97f98279bb
Cedric LOMBARDOT <cedric@donkeycode.com>
2021-11-19 11:53:56 +0100

    Migration

---
+++ src/Propel/migrations/PropelMigration_1637318846.php
@@ -0,0 +1,91 @@
+<?php
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1637318846.
+ * Generated on 2021-11-19 11:47:26 by root 
+ */
+class PropelMigration_1637318846 
+{
+    public $comment = '';
+
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        $connection_default = <<< 'EOT'
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `author`
+
+  CHANGE `name` `lastname` VARCHAR(255) NOT NULL,
+
+  ADD `firstname` VARCHAR(255) NOT NULL AFTER `lastname`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+EOT;
+
+        return array(
+            'default' => $connection_default,
+        );
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        $connection_default = <<< 'EOT'
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `author`
+
+  CHANGE `lastname` `name` VARCHAR(255) NOT NULL,
+
+  DROP `firstname`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+EOT;
+
+        return array(
+            'default' => $connection_default,
+        );
+    }
+
+}
