11336a0eedf39a0e67dce5a18c7338aa1ccddc7c
Cedric LOMBARDOT <cedric@donkeycode.com>
2021-11-19 07:36:51 +0100

    Init deps

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
